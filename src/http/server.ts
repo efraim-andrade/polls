fac6ef286d58133fdfeb3cfe8b50cdbf0612356c
Efraim Andrade <efraim.dev@gmail.com>
2024-02-08 00:18:51 -0300

    feat: polls / finish day two

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,21 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
-import z from "zod";
+import fastifyCookie from "@fastify/cookie";
 
+import { getPoll } from "../routes/get-poll";
+import { createPoll } from "../routes/create-poll";
+import { voteOnPoll } from "../routes/vote-on-poll";
 
 const app = fastify();
 
-const prisma = new PrismaClient()
+app.register(fastifyCookie, {
+  secret: "nlw-expert",
+  hook: "onRequest",
+});
 
-app.post("/polls", async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  })
-
-  const { title } = createPollBody.parse(request.body)
-
-  const poll = await prisma.poll.create({
-    data: { title }
-  })  
-
-  return reply.status(201).send({ pollId: poll.id })
-})
+app.register(getPoll);
+app.register(createPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running")
-})
+  console.log("HTTP server running");
+});
